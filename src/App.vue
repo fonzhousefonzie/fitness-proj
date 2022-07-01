ead71eeeca987c3f3e6d3262459f517e6179cc1c
fonzhousefonzie <harth.fonz@gmail.com>
2022-07-01 12:51:56 +0200

    Started, header completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TheHeader
+}
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto Condensed', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+.primary-btn{
+  background: #4154fe;
+  padding: 0.5rem 1.5rem;
+  color: white;
+  border-radius: 2rem;
+  font-size: 0.8rem;
+  transition: background 300ms ease-out;
+  &:hover{
+    background: #ec5a57;
+  }
+}
+
+.active{
+  color: white;
+}
+
+.new::after{
+  content: 'new';
+  color: #060606;
+  background: #fff941;
+  margin-left: 0.5rem;
+  padding: 0.2rem 0.3rem;
+  font-size: 0.8rem;
+  border-radius: 2px;
+  font-weight: bold;
+  vertical-align: middle;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+    <header class="d-flex align-items-center">
+        <div class="container d-flex justify-content-between align-items-center">
+            <img src="../assets/img/logo.png" alt="Avada Fitness Logo">
+            <div class="menu-container d-flex gap-4 align-items-center">
+                <ul class="d-flex list-unstyled align-items-center mb-0 gap-4">
+                    <li v-for="item in menu" :key="item" v-html="item"></li>
+                </ul>
+                <button class="primary-btn">Schedule a workout</button>
+                <div class="icons d-flex gap-4">
+                    <i class="fa fa-solid fa-cart-shopping"></i>
+                    <i class="fa fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            menu: [
+                "<span class='active'>Home</span>",
+                "<span>Services</span>",
+                "<span>About</span>",
+                "<span>Videos</span>",
+                "<span>Blog</span>",
+                "<span class='new'>Store</span>"
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        background: #060606;
+        min-height: 60px;
+        padding: 1.5rem;
+        color: #bcbcbc;
+    }
+
+    li, i{
+        cursor: pointer;
+        &:hover{
+            color: #709e9c;
+        }
+    }
+</style>
